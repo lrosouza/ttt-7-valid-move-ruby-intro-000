84d9c2df4feec608b8265ad143b306250b70d035
Lucas Souza <souza.lucas01@gmail.com>
2020-03-11 04:00:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,11 +3,11 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move?(board, index)
 		if position_taken?(board, index) == false && index.between?(0,8) == true
-		true
+		    true
 	  elsif position_taken?(board, index) == true
-		false
+		    false
 	  elsif index.between?(0, 8) == false
-		 false
+		    false
 	  end
 end
 
